5d645168493d0ca357a30a024936bc0daf45a717
Odin <odin.sev@gmail.com>
2021-10-11 00:19:26 -0400

    Lots of progress, probably best to sleep on this

--- src/alphabets.rs
+++ src/alphabets.rs
@@ -9,11 +9,21 @@ pub enum Observation<T> {
 }
 
 pub trait Alphabet {
-    type State;
+    type State: PartialEq;
     type StateIter: Iterator<Item = Self::State>;
 
     fn size() -> usize {
         Self::states().count()
     }
     fn states() -> Self::StateIter;
+    fn try_from_index(index: usize) -> Option<Self::State> {
+        Self::states()
+        .enumerate()
+        .find_map(|(i, x)| if i == index { Some(x) } else { None })
+    }
+    fn to_index(state: Self::State) -> usize {
+        Self::states()
+        .enumerate()
+        .find_map(|(i, x)| if x == state { Some(i) } else { None }).unwrap() // This will never fail
+    }
 }
--- src/markov.rs
+++ src/markov.rs
@@ -1,17 +1,18 @@
 // use crate::node::Node;
+use crate::alphabets::Alphabet;
+use crate::utils::functions::logsumexp;
+use argmax::Argmax;
 use itertools::Itertools;
-use std::collections::HashMap;
 use ndarray::prelude::*;
-use petgraph::graph::{NodeIndex, UnGraph};
+use petgraph::graph::{node_index as nidx, NodeIndex, UnGraph};
+use petgraph::visit::{depth_first_search, Control, DfsEvent};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::marker::PhantomData;
-use crate::utils::functions::logsumexp;
-use crate::alphabets::Alphabet;
-use argmax::Argmax;
 
-pub mod potentials;
 pub mod message_bank;
+pub mod potentials;
 
 use self::message_bank::MessageBank;
 use self::potentials::{EdgePotential, NodePotential};
@@ -37,16 +38,16 @@ where
 
     pub fn belief_propagation(&self) -> Array2<f64> {
         let num_nodes = self.graph.node_count();
-        let mut log_messages = MessageBank::new(X::size(), num_nodes, 0.0);//HashMap<(usize, NodeIndex, NodeIndex), f64> = HashMap::new();
+        let mut log_messages = MessageBank::new(X::size(), num_nodes, 0.0); //HashMap<(usize, NodeIndex, NodeIndex), f64> = HashMap::new();
         let d = 20; // TODO: Fix this
 
         let mut log_message_container: Vec<f64> = Vec::with_capacity(X::size());
         for _ in 0..d {
-            for n in (0..num_nodes).map(NodeIndex::new) {
+            for n in (0..num_nodes).map(nidx) {
                 for m in self.graph.neighbors(n) {
                     for (j, xj) in X::states().enumerate() {
                         for (i, xi) in X::states().enumerate() {
-                            let log_message_from_neighbors: f64 =  self
+                            let log_message_from_neighbors: f64 = self
                                 .graph
                                 .neighbors(m) // Loop over neighboring nodes
                                 .filter(|&k| k != n) // Exclude node j from the neighboring set
@@ -59,9 +60,14 @@ where
                                 "Should be an edge between nodes {:?} and {:?}, but isn't!",
                                 n, m
                             ));
-                            log_message_container.push(phi.phi(xi).ln() + psi.psi(xi, xj).ln() + log_message_from_neighbors);
+                            log_message_container.push(
+                                phi.phi(xi).ln()
+                                    + psi.psi(xi, xj).ln()
+                                    + log_message_from_neighbors,
+                            );
                         }
-                        *log_messages.message_mut(m, n).eval_state_mut(j) = logsumexp(log_message_container.as_slice());
+                        *log_messages.message_mut(m, n).eval_state_mut(j) =
+                            logsumexp(log_message_container.as_slice());
                         log_message_container.clear();
                     }
                 }
@@ -70,7 +76,7 @@ where
 
         let mut p = Array2::zeros((num_nodes, X::size()));
 
-        for j in (0..num_nodes).map(NodeIndex::new) {
+        for j in (0..num_nodes).map(nidx) {
             let phi = self.node_potential(j).unwrap();
             for (i, xi) in X::states().enumerate() {
                 let incoming_log_messages: f64 = self
@@ -90,19 +96,21 @@ where
         p
     }
 
-    pub fn min_sum(&self) -> Vec<X::State> {
+    pub fn min_sum(&self) -> HashMap<NodeIndex, X::State> {
         let num_nodes = self.graph.node_count();
-        let mut log_messages = MessageBank::new(X::size(), num_nodes, 0.0);//HashMap<(usize, NodeIndex, NodeIndex), f64> = HashMap::new();
-        let d = 20; // TODO: Fix this
+        let num_edges = self.graph.edge_count();
+        let mut log_messages = MessageBank::new(X::size(), num_nodes, 0.0); //HashMap<(usize, NodeIndex, NodeIndex), f64> = HashMap::new();
+        let mut backpointers: HashMap<(NodeIndex, NodeIndex), Vec<Option<usize>>> =
+            HashMap::with_capacity(2 * num_edges); //= MessageBank::new(X::size(), num_nodes, 0.0);//HashMap<(usize, NodeIndex, NodeIndex), f64> = HashMap::new();
 
-let mut backpointers: Vec<Vec<f64>> = Vec::with_capacity(num_nodes);
+        let d = 20; // TODO: Fix this
+        let mut log_message_container = Vec::with_capacity(X::size());
         for _ in 0..d {
             for n in (0..num_nodes).map(NodeIndex::new) {
                 for m in self.graph.neighbors(n) {
                     for (j, xj) in X::states().enumerate() {
-                        let mut log_message_container = Vec::new();
                         for (i, xi) in X::states().enumerate() {
-                            let log_message_from_neighbors: f64 =  self
+                            let log_message_from_neighbors: f64 = self
                                 .graph
                                 .neighbors(m) // Loop over neighboring nodes
                                 .filter(|&k| k != n) // Exclude node j from the neighboring set
@@ -115,16 +123,68 @@ let mut backpointers: Vec<Vec<f64>> = Vec::with_capacity(num_nodes);
                                 "Should be an edge between nodes {:?} and {:?}, but isn't!",
                                 n, m
                             ));
-                            log_message_container.push(phi.phi(xi).ln() + psi.psi(xi, xj).ln() + log_message_from_neighbors);
+                            log_message_container.push(
+                                phi.phi(xi).ln()
+                                    + psi.psi(xi, xj).ln()
+                                    + log_message_from_neighbors,
+                            );
                         }
-                        backpointers.push(log_message_container);
+                        let (max_index, max_val) = log_message_container
+                            .iter()
+                            .argmax()
+                            .expect("Log message container empty??");
+                        *log_messages.message_mut(m, n).eval_state_mut(j) = *max_val;
+                        backpointers.entry((m, n)).or_insert(vec![None; X::size()])[j] =
+                            Some(max_index);
+                        log_message_container.clear();
                     }
                 }
             }
         }
 
-        todo!()
+        // Traverse from node 0 (arbitraty) and propagate outwards
+        let mut map_estimates: HashMap<NodeIndex, X::State> = HashMap::with_capacity(num_nodes);
 
+        let start = nidx(0);
+        let phi_start = self
+            .node_potential(start)
+            .expect("Node 0 is not in graph??");
+        let mut max_marginal = Vec::with_capacity(X::size());
+
+        for (i, xi) in X::states().enumerate() {
+            let log_message_sum: f64 = self
+                .graph
+                .neighbors(start)
+                .map(|j| *log_messages.message(start, j).eval_state(i))
+                .sum();
+            max_marginal.push(phi_start.phi(xi) + log_message_sum);
+        }
+
+        let (map_index, _) = max_marginal.iter().argmax().unwrap();
+
+        let map_state = X::try_from_index(map_index).expect("Should be valid index!!");
+        map_estimates.insert(start, map_state);
+
+        let mut backpoint_containter = Vec::with_capacity(X::size());
+        depth_first_search(&self.graph, Some(start), |event| {
+            if let DfsEvent::TreeEdge(from, to) = event {
+                let xi = map_estimates[&from]; // I guess we have already recorded the max state of from node when doing dfs???
+
+                for (j, xj) in X::states().enumerate() {
+                    let log_neighboring_messages_sum: f64 = self
+                    .graph
+                    .neighbors(from)
+                    .filter(|&k| k != to)
+                    .map(|j| *log_messages.message(start, j).eval_state(X::to_index(xi)))
+                    .sum();
+                    backpoint_containter.
+                }
+
+            }
+            Control::<()>::Continue
+        });
+
+        map_estimates
     }
 
     fn node_potential(&self, index: NodeIndex) -> Option<&NP> {
@@ -132,9 +192,6 @@ let mut backpointers: Vec<Vec<f64>> = Vec::with_capacity(num_nodes);
     }
 
     fn edge_potential(&self, node1: NodeIndex, node2: NodeIndex) -> Option<&EP> {
-        self.graph.edge_weight(
-            self.graph
-                .find_edge(node1, node2)?,
-        )
+        self.graph.edge_weight(self.graph.find_edge(node1, node2)?)
     }
 }
